577279e5e7dbd2466398f54b127990a161af8f5b
Leonardo <Leonardofavio454@gmail.com>
2025-12-05 18:17:36 -0500

    Initial Commit

---
+++ CashControl/CashControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashControlApp.swift
+//  CashControl
+//
+//  Created by LEONARDO FAVIO  on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CashControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
